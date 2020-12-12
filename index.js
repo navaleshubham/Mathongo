0ed6d6c835fea0a73035c7b1147cbfa17146c517
shubham navale <shubham.navale12@gmail.com>
2020-12-12 22:27:08 +0530

    automatic scheduled added with 24 hrs gap

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer')
 const express = require('express')
+const https = require('https');
+const schedule = require('node-schedule');
 const app = express()
 const cors = require('cors')
 var fs = require('fs');
@@ -24,6 +26,8 @@ MongoClient.connect(uri, { useUnifiedTopology: true }, async function (err, clie
     Orders = client.db('mathongo').collection('Orders')
   }
 });
+var d=new Date()
+var t=new Date(d.getTime() + (24 * 60 * 60 * 1000))
 
 
 function ConvertToCSV(file) {
@@ -74,7 +78,128 @@ function sendmail(reciver, obj) {
   })
 
 }
-
+function getautocall(){
+  let csvfiledata = [];
+  let todaytrans = [];
+  let successtrans = [];
+  let todaysvolume = [];
+  let alldata = { '1': false, '2': false, '3': false, '4': false }
+  let date = new Date().toISOString().slice(0, 10) + '*'
+  let pipe = [
+    {
+      '$match': {
+        'createdAt.Date': {
+          '$regex': date
+        }
+      }
+    },
+    {
+      '$lookup': {
+        'from': 'Items',
+        'localField': 'item',
+        'foreignField': '_id',
+        'as': 'items'
+      }
+    }, {
+      '$unwind': {
+        'path': '$items'
+      }
+    }, {
+      '$project': {
+        'sl_no': {
+          '$sum': 1
+        },
+        'order_id': '$_id',
+        'payment_id': '$payment_id',
+        'createdAt': '$createdAt.Date',
+        'updatedAt': '$updatedAt.Date',
+        'item_id': '$item._id',
+        'item_name': '$item.title',
+        'coupon_amount': '$coupon',
+        'paid_status': '$paid',
+        'phone': '$phone',
+        'email': '$email',
+        'utm_params_source': '$utm_params.source',
+        'utm_params_medium': '$utm_params.medium',
+        'utm_params_campaign': '$utm_params.campaign',
+        'utm_params_term': '$utm_params.term'
+      }
+    }
+  ]
+  Orders.aggregate(pipe, (e, r) => {
+    r.forEach(element => {
+      csvfiledata.push(element)
+      alldata['1'] = true
+    });
+  })
+  const todays = [
+    {
+      '$match': {
+        'createdAt.Date': {
+          '$regex': date
+        }
+      }
+    }, {
+      '$count': 'count'
+    }
+  ];
+  Orders.aggregate(todays, (e, r) => {
+    r.forEach(element => {
+      todaytrans.push(element)
+      alldata['2'] = true
+    });
+  })
+  const done = [
+    {
+      '$match': {
+        'createdAt.Date': {
+          '$regex': date
+        },
+        'paid': true
+      }
+    }
+    , {
+      '$count': 'count'
+    }
+  ];
+  Orders.aggregate(done, (e, r) => {
+    r.forEach(element => {
+      successtrans.push(element)
+      alldata['3'] = true
+    });
+  })
+  const volume = [
+    {
+      '$match': {
+        'createdAt.Date': {
+          '$regex': date
+        },
+        'paid': true
+      }
+    }, {
+      '$project': {
+        'volume': {
+          '$sum': '$amount'
+        },
+        '_id': 0
+      }
+    }
+  ];
+  Orders.aggregate(volume, (e, r) => {
+    r.forEach(element => {
+      todaysvolume.push(element)
+      alldata['4'] = true
+    });
+  })
+  let datainter = setInterval(() => {
+    if (alldata['1'] && alldata['2'] && alldata['3'] && alldata['4']) {
+      let obj = { csvfiledata, todaytrans, successtrans, todaysvolume, 'result': done }
+      clearInterval(datainter)
+      ConvertToCSV(csvfiledata)
+      sendmail('shubham.navale17@comp.sce.edu.in', obj)
+    }
+  }, 1000)
+}
 app.get('/:email', (req, res) => {
   let csvfiledata = [];
   let todaytrans = [];
@@ -204,3 +329,6 @@ const server = app.listen(port, function () {
   console.log('Listening on port ' + port);
   console.log(server.address())
 });  
+var j = schedule.scheduleJob(t, function(){
+  getautocall()
+});
